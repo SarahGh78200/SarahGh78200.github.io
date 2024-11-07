b54e55e68d1a0a2e7e4b06b072456cb64cdd5934
SarahGh78200 <sarahghobrial78@gmail.com>
2024-11-07 10:23:40 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+<p align="center">
+    <img src="assets/image/logo.svg" width="256">
+</p>
+
+<h1 align="center">
+    Click Speed test ⚡
+</h1>
+
+<p align="center">
+    Clicks-per-second meter with dynamic counting and auto reset.
+</p>
+<p align="center">
+    <a href="https://github.com/FelixLuciano/clicks/actions/workflows/pages/pages-build-deployment"><img src="https://github.com/FelixLuciano/clicks/actions/workflows/pages/pages-build-deployment/badge.svg" alt="pages-build-deployment"></a>
+</p>
+
+<br>
+
+![App screenshot](assets/image/screenshot.jpg)
+
+
+## Features
+
+The app features a dynamic counting system, which means that it continuously updates the clicks-per-second count as the user clicks in real-time. It also has an automatic reset feature, which clears the count after a certain amount of time has passed, allowing users to track their CPS over multiple sessions. The app also includes a rate history, that stores and displays the user's best clicking speed, allowing them to track their progress and see how their clicking speed has changed.
+
+This app is useful if you want to improve your clicking speed and efficiency, and can be used for a variety of purposes such as gaming.
+
+## Check it out
+
+Here is the following link to [see it online](https://github.lucianofelix.com.br/clicks/).
+It was made based on [this Pen at CodePen](https://codepen.io/FelixLuciano/pen/MWavXmy).
+
+
+## License
+
+This project is [MIT licensed](LICENSE)!
---
+++ script.js
@@ -0,0 +1,121 @@
+const clickSpeedTest = () => {
+    return {
+        clicks: 0,
+        startTime: 0,
+        endTime: 0,
+        activeTime: 0,
+        cps: 0,
+        instantCps: 0,
+        cpsMax: 0,
+        storeTimeout: null,
+
+        get time() {
+            return new Date().getTime()
+        },
+
+        get idleTime() {
+            return this.time - this.endTime
+        },
+
+        get activeTimeDisplay() {
+            return getDisplay(this.activeTime / 1000, 1, 's')
+        },
+
+        get cpsDisplay() {
+            return getDisplay(this.cps)
+        },
+
+        get instantCpsDisplay() {
+            return getDisplay(this.instantCps)
+        },
+
+        get cpsMaxDisplay() {
+            return getDisplay(this.cpsMax, 4)
+        },
+
+        restart() {
+            this.clicks = this.cps = this.instantCps = this.cpsMax = 0
+            this.startTime = this.time
+        },
+
+        mouseDown() {
+            this.activeTime = this.time - this.startTime
+            this.clicks++
+
+            if (this.idleTime > 1000)
+                this.restart()
+
+            if (this.activeTime > 100) {
+                this.instantCps = 1000 / this.idleTime
+                this.cps = this.clicks / this.activeTime * 1000
+            }
+
+            if (this.activeTime > 1000)
+                this.cpsMax = Math.max(this.cps, this.cpsMax)
+        },
+
+        mouseUp() {
+            this.endTime = this.time
+
+            if (this.cpsMax > 0) {
+                clearTimeout(this.storeTimeout)
+
+                this.storeTimeout = setTimeout(() => this.store(), 1200)
+            }
+        },
+
+        store() {
+            this.$dispatch('store', {
+                value: this.cpsMax
+            })
+        },
+    }
+}
+
+const history = () => {
+    const STORAGE_KEY = "com.lucianofelix.clickspeedtest"
+
+    return {
+        history: [],
+
+        get serializedHistory() {
+            return JSON.stringify(this.history.slice(0, 20))
+        },
+
+        insert(value) {
+            let index
+
+            for (index = 0; index < this.history.length; index++)
+                if (value > this.history[index])
+                    break
+
+                this.history.splice(index, 0, value)
+        },
+
+        fetch() {
+            const storageData = localStorage.getItem(STORAGE_KEY)
+
+            if (storageData != null)
+                this.history = JSON.parse(storageData)
+            else
+                this.commit()
+        },
+
+        commit() {
+            const a = this.serializedHistory
+            localStorage.setItem(STORAGE_KEY, a)
+        },
+
+        store(event) {
+            this.insert(event.detail.value)
+            this.commit()
+        },
+    }
+}
+
+
+function getDisplay(num, fix = 2, unit = '') {
+    const [intVal, floatVal] = num.toFixed(fix).split('.')
+
+    return `<span class="value__int">${intVal}</span>.<span class="value__float">${floatVal}</span><span class="value__unit"> ${unit}</span>`
+}
